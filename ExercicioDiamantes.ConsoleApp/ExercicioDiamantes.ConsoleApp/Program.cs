644678bcc84f2fe1fa402a68e3c42e2cff1e4a87
Pedro Nunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-15 18:49:09 -0300

    Add files via upload

---
+++ ExercicioDiamantes.ConsoleApp/ExercicioDiamantes.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExercicioDiamantes.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, metade, qntEspacos, qntX = 1;
+            do {
+                Console.Write("Digite o número ímpar: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }while (numero % 2 == 0);
+
+            qntEspacos = (numero - 1) / 2;
+            metade = numero / 2;
+            for (int i = 0; i <= metade; i++)
+            {
+                for (int j = 0; j < qntEspacos; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < qntX; k++)
+                {
+                    Console.Write("x");
+                }
+                Console.WriteLine();
+                qntX = qntX + 2;
+                qntEspacos = qntEspacos - 1;   
+            }
+            qntX = numero;
+            qntEspacos = 0;
+            for(int i = 0;i < metade; i++)
+            {
+                qntX = qntX - 2;
+                qntEspacos = qntEspacos + 1;
+                for (int j = 0; j < qntEspacos; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < qntX; k++)
+                {
+                    Console.Write("x");
+                }
+                Console.WriteLine();
+                
+            }
+        
+        }
+    }
+}
